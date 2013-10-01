9e210d362224a92c55d059eaaed94123edb1676d
Martin Tajur <martin@tajur.ee>
2013-10-01 14:54:47 +0300

    version bump: improved error handling, added ability to store new fields when using .set(), using standard fetch library now given from npm

--- examples/list-deals.js
+++ examples/list-deals.js
@@ -12,9 +12,11 @@ pipedrive.Filters.getAll({ type: 'deals' }, function(filtersListErr, filtersList
 		console.log('Fetching deals that match filter "' + filtersList[0].get('name') + '"...');
 
 		pipedrive.Deals.getAll({ filter_id: filtersList[0].get('id'), start: 0, limit: 5 }, function(dealsListErr, dealsList) {
+			if (dealsListErr) console.log(dealsListErr);
 			_.each(dealsList, function(deal) {
 				deal.getActivities(function(err, activities) {
-				if(activities == null) activities = new Array();
+					if (err) console.log(err);
+					if(activities == null) activities = new Array();
 					console.log('* ' + deal.get('title') + ' (worth ' + deal.get('value') + ' ' + deal.get('currency') + ', has ' + activities.length + ' activit' + (activities.length != 1 ? 'ies' : 'y') + ')');	
 				});
 			});
