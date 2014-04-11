8c3fbb95f72e277c6c9760c14ffba5d878a14b43
Martin Tajur <martin@tajur.ee>
2014-04-11 12:51:02 +0300

    improved the deals-list-reason-search example and added pagination to SearchResults.fields params in readme

--- examples/search-deals-by-lost-reason.js
+++ examples/search-deals-by-lost-reason.js
@@ -11,7 +11,16 @@ var Pipedrive = require(__dirname + '/../index');
 var pipedrive = new Pipedrive.Client(process.argv[2]);
 var _ = require('lodash');
 
-pipedrive.SearchResults.field({ term: process.argv[3], exact_match: false, field_type: 'dealField', field_key: 'lost_reason', return_item_ids: true, limit: 10 }, function(dealsListErr, dealsList) {
+var reqParams = {
+	term: process.argv[3],
+	exact_match: false,
+	field_type: 'dealField',
+	field_key: 'lost_reason',
+	return_item_ids: true,
+	limit: 10
+};
+
+pipedrive.SearchResults.field(reqParams, function(dealsListErr, dealsList) {
 	if (dealsListErr) console.log(dealsListErr);
 	_.each(dealsList, function(deal) {
 		pipedrive.Deals.get(deal.id, function(err, fullDealData) {
@@ -19,7 +28,7 @@ pipedrive.SearchResults.field({ term: process.argv[3], exact_match: false, field
 			deal.getActivities(function(err, activities) {
 				if (err) console.log(err);
 				if(activities == null) activities = new Array();
-				console.log('* ' + fullDealData.get('title') + ' (Lost reason: ' + fullDealData.lost_reason + ') was worth ' + fullDealData.get('value') + ' ' + fullDealData.get('currency') + ', has ' + activities.length + ' activit' + (activities.length != 1 ? 'ies' : 'y'));
+				console.log('* ' + fullDealData.get('title') + ' (Lost reason: ' + fullDealData.lost_reason + ') was worth ' + fullDealData.get('value') + ' ' + fullDealData.get('currency') + ', had ' + activities.length + ' activit' + (activities.length != 1 ? 'ies' : 'y'));
 			});
 		});
 	});
