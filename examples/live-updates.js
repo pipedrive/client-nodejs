e42f90ef660f829477c2ef7c88edffa285d78e4f
Martin Tajur <martin@tajur.ee>
2015-03-21 14:53:30 +0200

    rename listener->channel, improve event handling, rearrange listener patterns (object.action)

--- examples/live-updates.js
+++ examples/live-updates.js
@@ -7,12 +7,12 @@ var Pipedrive = require(__dirname + '/../index');
 var pipedrive = new Pipedrive.Client(process.argv[2]);
 var _ = require('lodash');
 
-pipedrive.on('added.deal', function(event, data) {
+pipedrive.on('deal.added', function(event, data) {
 	console.log('Deal ' + event.meta.id + ' was added ('+data.current.title+', worth '+data.current.value+' '+data.current.currency+')');
 	pipedrive.removeAllListeners();
 	process.exit();
 });
 
-pipedrive.on('listening', function() {
+pipedrive.on('connect', function() {
 	pipedrive.Deals.add({ title: 'Live deal', value: 10000, currency: 'EUR' });
 });
