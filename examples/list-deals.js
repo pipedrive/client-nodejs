ba93c5b0c7f2de8e936d25a997d73c927a3c3a92
Martin Tajur <martin@tajur.ee>
2016-04-25 12:39:13 +0300

    added a comment about fetching some deals matching a filter

--- examples/list-deals.js
+++ examples/list-deals.js
@@ -1,3 +1,19 @@
+/*!
+ * This basic example shows how to fetch some deals
+ * matching a specific filter from Pipedrive account.
+ *
+ * It starts by fetching the first deals filter from the
+ * account, it then fetches 5 deals matching to this
+ * filter.
+ *
+ * It will then display a list of found deals along
+ * with the values and with how many activities they are
+ * associated with.
+ *
+ * Usage:
+ * node list-deals.js APITOKEN
+**/
+
 if (!process.argv[2]) {
 	process.stderr.write('Please provide API token!' + "\n");
 	process.exit();
