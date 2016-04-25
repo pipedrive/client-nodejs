401506aef944816a1807b926e753d372c9739ca7
Martin Tajur <martin@tajur.ee>
2016-04-25 12:44:18 +0300

    added a comment about live listeners example

--- examples/live-updates.js
+++ examples/live-updates.js
@@ -1,3 +1,34 @@
+/*!
+ * This basic example shows how to use live,
+ * EventEmitter-like listener functions bound to
+ * actual events happening on your Pipedrive account.
+ *
+ * It starts by attaching a function to all
+ * 'deal.added' events happening on Pipedrive.
+ * The syntax for event names is the following:
+ * '{objectName}.{eventName}' where eventName can
+ * be any of the following: added, deleted, updated.
+ *
+ * It then further sets a handler function to a
+ * special 'connect' event which is triggered when
+ * the live event handlers functionality has 
+ * established a websocket connection. When that is
+ * done, the script adds a new deal, with the title
+ * "Live deal". Once the deal is added, the original
+ * callback for the 'deal.added' event gets invoked.
+ *
+ * This example shows you how to build real-time
+ * integrations using EventEmitter style in Node.js
+ * apps along with your Pipedrive account data.
+ *
+ * Important notice: in order for live listeners to
+ * work, you need to set { strictMode: true } when
+ * invoking the new Pipedrive.Client(). See below.
+ *
+ * Usage:
+ * node live-updates.js APITOKEN
+ */
+
 if (!process.argv[2]) {
 	process.stderr.write('Please provide API token!' + "\n");
 	process.exit();
