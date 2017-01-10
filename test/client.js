06dfd3a23faea468dce97423c4325a4722d6ad18
Martin Tajur <martin@tajur.ee>
2017-01-10 15:42:30 +0200

    use blueprint in test

--- test/client.js
+++ test/client.js
@@ -4,15 +4,20 @@ describe('client module', function() {
 
 	it('should expose some of the main object types', function() {
 		var Pipedrive = require('./..'),
-			pipedrive = new Pipedrive.Client('apitoken');
+			pipedrive = new Pipedrive.Client('apitoken'),
+			blueprint = require('./../lib/blueprint');
 
-		should(pipedrive.Deals).be.an.Object();
-		should(pipedrive.Persons).be.an.Object();
-		should(pipedrive.Organizations).be.an.Object();
-		should(pipedrive.Activities).be.an.Object();
-		should(pipedrive.Products).be.an.Object();
-		should(pipedrive.Files).be.an.Object();
-		should(pipedrive.Users).be.an.Object();
+		// iterate through all top level objects which should be exposed, e.g. pipedrive.Deals, ...
+		blueprint.apiObjects.forEach(function(obj) {
+			should(pipedrive[obj.substr(0,1).toUpperCase() + obj.substr(1)]).be.an.Object();
+		});
+	});
+
+	it('should expose .on() listener in strict mode', function() {
+		var Pipedrive = require('./..'),
+			pipedrive = new Pipedrive.Client('apitoken', { strictMode: true });
+
+		should(pipedrive.on).be.a.Function();
 	});
 
 });
