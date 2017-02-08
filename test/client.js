1e69aab37d00d56a526c862311472a0af244c1f8
deathbearbrown <deathbearbrown@gmail.com>
2017-02-08 07:47:00 -0800

    Pipedrive.getALL passing wrong parameters

--- test/client.js
+++ test/client.js
@@ -1,23 +1,31 @@
-var should = require('should');
+var should = require('should/as-function'),
+	Pipedrive = require('./..'),
+	blueprint = require('./../lib/blueprint');
 
 describe('client module', function() {
+	var client,
+		strictClient;
+
+	beforeEach(function() {
+		client = new Pipedrive.Client('apitoken');
+		strictClient = new Pipedrive.Client('apitoken', { strictMode: true });
+	});
 
 	it('should expose some of the main object types', function() {
-		var Pipedrive = require('./..'),
-			pipedrive = new Pipedrive.Client('apitoken'),
-			blueprint = require('./../lib/blueprint');
-
 		// iterate through all top level objects which should be exposed, e.g. pipedrive.Deals, ...
 		blueprint.apiObjects.forEach(function(obj) {
-			should(pipedrive[obj.substr(0,1).toUpperCase() + obj.substr(1)]).be.an.Object();
+			should(client[obj.substr(0,1).toUpperCase() + obj.substr(1)]).be.an.Object();
 		});
 	});
 
 	it('should expose .on() listener in strict mode', function() {
-		var Pipedrive = require('./..'),
-			pipedrive = new Pipedrive.Client('apitoken', { strictMode: true });
-
-		should(pipedrive.on).be.a.Function();
+		should(strictClient.on).be.a.Function();
 	});
 
+	it('pipedrive.getAll should error if it is passed a bad resource', function(done) {
+		client.getAll('bananas', function(err) {
+			should(err).be.an.instanceOf(Error, 'Did not error on bad resource');
+			return done();
+		});
+	});
 });
