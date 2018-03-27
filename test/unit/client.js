af34614d68f4de6c9f05166bad69a9922885817d
Artjom Kurapov <artkurapov@gmail.com>
2018-03-27 16:39:19 +0300

    Formatting & unit test naming/grouping improvement

--- test/unit/client.js
+++ test/unit/client.js
@@ -2,27 +2,36 @@ var should = require('should/as-function'),
 	Pipedrive = require('./../..'),
 	blueprint = require('./../../lib/blueprint');
 
-describe('client module', function() {
+describe('client module', function () {
 
-	beforeEach(function() {
+	beforeEach(function () {
 		client = new Pipedrive.Client('apitoken');
-		strictClient = new Pipedrive.Client('apitoken', { strictMode: true });
+		strictClient = new Pipedrive.Client('apitoken', {strictMode: true});
 	});
 
-	it('should expose some of the main object types', function() {
-		// iterate through all top level objects which should be exposed, e.g. client.Deals, ...
-		blueprint.apiObjects.forEach(function(obj) {
-			should(client[obj.substr(0,1).toUpperCase() + obj.substr(1)]).be.an.Object();
+	it('should expose main API objects', function () {
+		should(client.Activities).be.an.Object();
+		should(client.Deals).be.an.Object();
+
+		// iterate through all
+		blueprint.apiObjects.forEach(function (obj) {
+			should(client[obj.substr(0, 1).toUpperCase() + obj.substr(1)]).be.an.Object();
 		});
 	});
 
-	it('should expose .on() listener in strict mode', function() {
-		should(strictClient.on).be.a.Function();
+	describe('client.on()', function () {
+		it('should be defined in strict mode', function () {
+			should(strictClient.on).be.a.Function();
+		});
+
+		it('should not be defined in regular mode', function () {
+			should(client.on).be.a.Undefined();
+		});
 	});
 
-	it('pipedrive.getAll should error if it is passed a bad resource', function(done) {
-		client.getAll('bananas', function(err) {
-			should(err).be.an.instanceOf(Error, 'Did not error on bad resource');
+	it('client.getAll() should throw error if non-existant resource is requested', function (done) {
+		client.getAll('bananas', function (error) {
+			should(error.message).equal('bananas is not supported object type for getAll()');
 			return done();
 		});
 	});
