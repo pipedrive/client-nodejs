3723c77312689d2cac9d2a89c21cf797c3e8f0ee
Youssef <youssef.saber@pipedrive.com>
2025-01-17 11:09:18 +0200

    GRAM-3870 add tests for deal

--- test/functional/stubs/deals.stubs.ts
+++ test/functional/stubs/deals.stubs.ts
@@ -7,6 +7,15 @@ export const DealsApiMock = {
 	getDeals<T>(resolves: Resolves<T>, status = 200) {
 		return api.get('/v1/deals').reply(status, resolves);
 	},
+	getDeal<T>(id:number, resolves: Resolves<T>, status = 200) {
+		return api.get(`/v1/deals/${id}`).reply(status, resolves);
+	},
+	deleteDeal<T>(id:number, resolves: Resolves<T>, status = 200) {
+		return api.delete(`/v1/deals/${id}`).reply(status, resolves);
+	},
+	updateDeal<T>(id:number, resolves: Resolves<T>, status = 200) {
+		return api.put(`/v1/deals/${id}`).reply(status, resolves);
+	},
 	createDeal<T>(resolves: Resolves<T>, status = 201) {
 		return api.post('/v1/deals').reply(status, resolves);
 	},
