a112dce3da7fd5aca23f6fea50f26b266162a85d
Youssef <youssef.saber@pipedrive.com>
2025-01-29 12:51:50 +0200

    GRAM-3870 remove deprecated tests

--- test/functional/automatic-token-refresh.test.js
+++ test/functional/automatic-token-refresh.test.js
@@ -6,8 +6,7 @@ const oauth2 = {
 	clientId: 'fakeClientId',
 	clientSecret: 'fakeClientSecret',
 	redirectUri: 'https://example.org',
-	accessToken: 'fakeAccessToken',
-	refreshToken: 'fakeRefreshToken',
+	basePath: 'http://localhost/v1',
 };
 
 const server = getMockServer(oauth2);
@@ -31,41 +30,6 @@ describe('automatic token refresh in api calls', () => {
 		const pdClient = new ApiClient();
 		pdClient.basePath = 'localhost/v1';
 		pdClient.authentications.oauth2 = { ...oauth2, expiresAt: 100 };
-
-		const users = await new UsersApi(pdClient).getUsers();
-
-		expect(pdClient.authentications.oauth2.accessToken).toEqual('freshAccessToken');
-
-		expect(users).toEqual({
-			success: true,
-			data: [
-				{
-					id: 1,
-					name: 'John Doe',
-				},
-			],
-		});
-	});
-
-	it('should throw if incorrect User-Agent request header in api call', async () => {
-		const pdClient = new ApiClient();
-		pdClient.basePath = 'localhost/v1';
-		pdClient.authentications.oauth2 = { ...oauth2 };
-		pdClient.defaultHeaders = {
-			'User-Agent': 'Wrong-User-Agent',
-		};
-
-		try {
-			expect(await new UsersApi(pdClient).getUsers()).toThrow();
-		} catch (error) {
-			expect(error.errorCode).toEqual(400);
-		}
-	});
-
-	it('should refresh token and retry api call if status code is 401', async () => {
-		const pdClient = new ApiClient();
-		pdClient.basePath = 'localhost/v1';
-		pdClient.authentications.oauth2 = { ...oauth2 };
 
 		const users = await new UsersApi(pdClient).getUsers();
 
